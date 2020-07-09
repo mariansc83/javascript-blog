712a418635f7fd0e8c3acc1588ff4167d1e705d2
Marian-Rozycki <67416143+Marian-Rozycki@users.noreply.github.com>
2020-07-09 14:56:43 +0200

    Add class active to clicked link

--- js/script.js
+++ js/script.js
@@ -5,18 +5,23 @@
 //     console.log('links:', links);
 // });
 
-const titleClickHandler = function () {
+const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
-    /* remove class 'active' from all article links  */
+    console.log('clickedElement (with plus): ' + clickedElement);
+
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
+
+
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        clickedElement.classList.add('active');
     }
 
-    /* add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
@@ -33,8 +38,4 @@ const articles = document.querySelectorAll('.posts .post');
 
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-}
-
-for (let article of articles) {
-    article.addEventListener('click', titleClickHandler);
 }
